0c81e11e069130c1caf8ce030cdbd4802b424c17
Roko131 <nirrako@gmail.com>
2014-10-15 12:58:40 +0300

    up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
+  helper ['spree/products', 'spree/base']
+  helper_method ["current_currency"]
   def test1
   end
 
   def test2
+  end
+  
+  private
+  
+  def current_currency
+     Spree::Config[:currency]
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,9 @@ module Store2
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-
+    config.assets.paths << "#{Rails.root}/app/assets/store/colors"
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
